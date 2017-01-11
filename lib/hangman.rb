5debc6f40aa89e45e0fa7f89614dcff90ec709f4
Taras <tarasyakovchuk@gmail.com>
2017-01-10 23:19:52 -0800

    added functionality. now users are able to save and load game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,7 @@
+require "yaml"
+
 class Game
-	attr_accessor :dictionary, :word, :guess, :misses, :show_word
+	attr_accessor :dictionary, :word, :guess, :misses, :show_word, :attempt, :saved_game
 
 	def initialize
 		@dictionary = File.readlines("dictionary.txt")
@@ -26,11 +28,10 @@ class Game
 	end
 
 	def prompt(attempt)
-		puts "Attempt number #{attempt+1}"
+		puts "Attempt number #{@attempt+1}"
 		show(@show_word)
 		print "Guess a letter: "
 		@guess = gets.chomp
-		p @guess
 		puts "Misses: #{@misses}"
 	end
 
@@ -44,6 +45,7 @@ class Game
 		(@word.length-1).times do |counter|
 			counter1 = counter1 + 1
 			if @word[counter] == @guess
+				puts "You got it! There is a letter '#{@word[counter]}' in the word."
 				@show_word[counter] = @word[counter]
 
 			elsif @word[counter] != @guess
@@ -54,28 +56,118 @@ class Game
 
 		if counter1 == counter2
 			@misses.push(@guess)
+			puts "I'm sorry, letter '#{@guess}' is not present in the word."
 		end
 		
 
 	end
 
+	def yaml_dump
+		File.open("game.yaml", "w") do |file|
+			file.puts YAML::dump(self)
+		end
+	end
+
+	def yaml_load
+		File.open("game.yaml", "r") do |obj|
+			@saved_game=YAML::load(obj)
+			
+		end
+	end
 
 
-	def game_on
-		attempt = 0
+	def new_or_saved
+		print "Enter 1 start a new game, enter 2 to load saved game: "
+		answer = gets.chomp
+		answer = answer.to_i
+	end
+
+	def save_game
+		print "Enter 1 to save the game, enter 2 to quit, or hit 'enter' to continue: "
+		answer = gets.chomp
+		answer = answer.to_i
+
+	end
+
+
+	def new_game
+		@attempt = 0
 		
-		while !won && (attempt <= @word.length+1)
-		
-			prompt(attempt)
+		while !won && (@attempt <= @word.length+1)
+			
+			case save_game
+				when 1
+					yaml_dump
+				when 2
+					exit
+			end
+
+			prompt(@attempt)
 			
 			check_guess
 
-			attempt = attempt + 1
+			@attempt = @attempt + 1
 		
 		end
 
 	end	
 
+	
+	def load_game
+		if yaml_load
+		
+			while !(@saved_game.show_word.join("") == @saved_game.word.chomp) && (@saved_game.attempt <= @saved_game.word.length+1)
+			
+				#prompt
+					puts "Attempt number #{@saved_game.attempt+1}"
+					puts "Word: #{@saved_game.show_word}"
+					print "Guess a letter: "
+					@saved_game.guess = gets.chomp
+					puts "Misses: #{@saved_game.misses}"
+				#prompt
+				
+				#check guess
+					counter1 = 0
+					counter2 = 0
+					(@saved_game.word.length-1).times do |counter|
+						counter1 = counter1 + 1
+						if @saved_game.word[counter] == @saved_game.guess
+							puts "You got it! There is a letter '#{@saved_game.word[counter]}' in the word."
+							@saved_game.show_word[counter] = @saved_game.word[counter]
+
+						elsif @saved_game.word[counter] != @saved_game.guess
+							counter2 = counter2 + 1
+						end
+					
+					end
+
+					if counter1 == counter2
+						@saved_game.misses.push(@saved_game.guess)
+						puts "I'm sorry, letter '#{@saved_game.guess}' is not present in the word."
+					end		
+
+				#check guess
+
+				@saved_game.attempt = @saved_game.attempt + 1
+		
+			end
+		else
+			puts "There are no saved games to load. Bye!"
+			exit
+		end
+	end
+
+	def start
+		case new_or_saved
+			when 1
+				new_game
+			when 2
+				load_game
+		end
+	end
+
+
 end
 
 game1 = Game.new
+game1.start
